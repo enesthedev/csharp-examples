6532d845a9f77223adb728d98c491358badf5e52
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2022-03-25 18:02:46 +0100

    Add: input values personName, personWeight, personLength to main method
    Add: ValidationError method for generic error messages
    
    Add: Type validations of input values

--- BasicBodyMassIndexCalculator/Program.cs
+++ BasicBodyMassIndexCalculator/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+using BasicBodyMassIndexCalculator.Common.Classes;
 
 namespace BasicBodyMassIndexCalculator
 {
@@ -6,7 +8,66 @@ namespace BasicBodyMassIndexCalculator
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Boy/Kilo Endeksi Programı");
+
+            Console.Write("Lütfen adınızı giriniz:");
+            string personName = Convert.ToString(Console.ReadLine());
+
+            if (!Regex.IsMatch(personName, "^[A-Za-z]+[A-Za-z ]*$"))
+            {
+                ValidationError("İsminiz geçersiz karakterler içeriyor. Lütfen geçerli bir isim giriniz.", personName, "Enes Bayraktar");
+                return;
+            }
+
+            
+            Console.Write("Lütfen kilonuzu giriniz:");
+            string personWeight = Convert.ToString(Console.ReadLine());
+
+            if (! Int32.TryParse(personWeight, out int numericValue))
+            {
+                ValidationError("Kilonuz sayılardan oluşmalıdır. Lütfen geçerli bir kilo giriniz.", personWeight, "110");
+                return;
+            }
+
+            Console.Write("Lütfen boyunuzu metre cinsinden giriniz:");
+            string personLength = Convert.ToString(Console.ReadLine());
+
+            if (personLength.IndexOf(".") == -1 | !Double.TryParse(personLength, out double doubleValue))
+            {
+                ValidationError("Lütfen boyunuzu santimetre cinsinden giriniz.", personLength, "1.89");
+                return;
+            }
+
+            if (Convert.ToInt32(personLength.Substring(0, 1)) > 2)
+            {
+                ValidationError("Lütfen boyunuzu doğru giriniz.", personLength, "1.70");
+                return;
+            }
+
+            Person person = new Person(
+                personName,
+                Convert.ToInt32(personWeight),
+                Convert.ToDouble(personLength)
+            );
+
+            int personMassIndex = person.CalculateMassIndex();
+
+            if (-10 <= personMassIndex && personMassIndex <= 10)
+            {
+                Console.WriteLine("Kilonuz normal");
+            } else if (10 <= personMassIndex)
+            {
+                Console.WriteLine("Kilonuz referans değerlerinin altında, kilo almalısınız.");
+            } else
+            {
+                Console.WriteLine("Kilonuz referans değerlerin üstünde, kilo vermelisiniz.");
+            }
+
+        }
+
+        public static void ValidationError(string errorMessage, string errorValue, string defaultValue)
+        {
+            Console.WriteLine($"\nHata: {errorMessage}\nGirilen değer:{errorValue}\nÖrnek değer:{defaultValue}");
         }
     }
 }
