8b1ed4f280102f2d4b6e73ad5d79bf532dfba0ea
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2022-03-25 18:55:18 +0100

    Add: comments to IPerson interface and Person classes

--- BasicBodyMassIndexCalculator/Common/Classes/Person.cs
+++ BasicBodyMassIndexCalculator/Common/Classes/Person.cs
@@ -3,6 +3,19 @@ using BasicBodyMassIndexCalculator.Common.Interfaces;
 
 namespace BasicBodyMassIndexCalculator.Common.Classes
 {
+    /*
+        Person sınıfı
+
+        bu sınıf Vücut kitle endeksi hesaplamada sistemin ileride geliştirilip birden çok insanın aynı anda hesaplanması gerekeceği durumlara karşın hazırlandı.
+        sınıf temel olarak IPerson arayüzünü kullanıyor, isim, kilo ve boy değişkenlerine sahip. Bu değişkenlerin düzenlenebilmesi için getter ve setterları mevcut.
+
+        Vücut kitle endeksinin hesaplabilmesi için ise CalculateMassIndex isimli bir metod tanımladım. Bu metod basitce kullanıcının kilosundan boyunu çıkartıyor.
+        Peki bu sınıf boyu double tipinden nasıl kilodan çıkartılacak referans değere ulaştırıyor dersek. Önce double tiğini Convert.ToIn32 ile integer değere dönüştürüyoruz.
+
+        Bu sayede örnek değerimiz 1.89, 189 olarak tanımlanmış oluyor. Bu değerden 100cm yani 1 metre çıkarmamız ise denklemin referans değerini sağlayan parça oluyor.
+        100cm çıkarmamızın sebebi ise bir insanın boy/kitle indeksi hesaplanırken 1.89m boyunda olan birinin 89+10 ile 89-10 aralığında olmasının normal değerler içinde kabul edilmesidir.
+        2.10m olan birisi için bu değer 110+10 ila 110-10 arasında olması beni boy değerinden 1m çıkartmaya iten şey. Daha dinamik bir yolu bulunabilir mi bilmiyorum.
+    */
     public class Person : IPerson
     {
         private string _name { get; set; }
--- BasicBodyMassIndexCalculator/Common/Interfaces/IPerson.cs
+++ BasicBodyMassIndexCalculator/Common/Interfaces/IPerson.cs
@@ -1,6 +1,14 @@
 ﻿using System;
 namespace BasicBodyMassIndexCalculator.Common.Interfaces
 {
+    /*
+        Dersde gördük mü bilmiyorum ancak sınıfın değerlerinin doğrulanabilmesi için bir aracı yazmam gerekiyordu aksi halde
+        obje tabanlı programlamanın prensiblerine ters bir işlem yapılmış olucaktı.
+
+        Bu kod olmasa yine sınıfı yaratabilir ve kodu çalıştırabilirdik ancak best-practise yani yapılması gerektiği gibi olmamış olurdu.
+        ayriyetten person'ın dışında bir çalışan sınıfı gibi insanların bilgilerinin depolanması gerekeceği sınıflar da olmuş olsaydı IPerson yerine IHuman diyip diğer sınıfları
+        buradan türetebilirdik.
+    */
     public interface IPerson
     {
         string Name { get; set; }
